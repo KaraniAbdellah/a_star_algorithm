9384981fc1b006f8f16521e88ac6e1e15f730acb
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-11 23:55:05 +0100

    add generators

--- basics.py
+++ basics.py
@@ -33,6 +33,38 @@ print(info.values())
 
 
 
+# tuples
+# i can not change the tupes after affectation
+print("-------------------")
+coordinates = (1, 2)
+# coordinates[0] = 10 # not support iteem assignement
+print(coordinates)
+
+
+# Generator Express
+'''
+    use () for use generators --> we create just a generate object
+    to generate next item from this object use next()
+        next(): generate item from sequence
+'''
+print("-------------------")
+my_list = [x for x in range(1, 3)]
+my_generator = (x for x in range(1, 10))
+
+print(my_list) # [1, 2]
+print(my_generator) # just address --> we store anything
+
+print(next(my_generator)) # 1
+print(next(my_generator)) # 2
+
+for item in my_generator:
+    print(item)
+
+
+
+
+
+
 
 
 
