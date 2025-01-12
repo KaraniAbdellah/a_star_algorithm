79a214fd76990fc296480f55f4fc85612c8650ef
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-12 20:11:13 +0100

    start directed graph

--- graphs.py
+++ graphs.py
@@ -28,8 +28,8 @@ edges = [
 ]
 
 G = Graph(nodes, edges)
-print(G.edges)
-print(G.nodes)
+# print(G.edges)
+# print(G.nodes)
 
 
 # Create graph with Adjacency list
@@ -39,16 +39,46 @@ def adjacency_dict(graph):
     return the adjacency list representation of the graph
     '''
     adj = {node: [] for node in graph.nodes}
+    print(adj) # {'A': [], 'B': [], 'C': [], 'D': []}
     for edge in graph.edges:
         node1, node2 = edge[0], edge[1]
         adj[node1].append(node2)
         adj[node2].append(node1)
     return adj
 
-print(adjacency_dict(G))
+# adjacency_dict(G)
 
 
 # Create graph With Adjacency Matrix
+nodes = range(4)
+edges = [
+    (0, 1),
+    (1, 1),
+    (0, 2),
+    (2, 0),
+    (0, 3),
+    (3, 0),
+    (3, 1),
+    (3, 2)
+]
+G = Graph(nodes, edges)
+def adjacency_matrix(graph):
+    '''
+    return the adjacency matrix of the graph
+    Assumes that graph.nodes is equivalent  to range(len(graph.node))
+    '''
+    adj = [[0 for node in graph.nodes] for node in graph.nodes]
+    # [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    # print(adj)
+    for edge in graph.edges:
+        node1, node2 = edge[0], edge[1]
+        adj[node1][node2] += 1
+        adj[node2][node1] += 1
+    return adj
+
+print(adjacency_matrix(G))
+
+
 
 
 
