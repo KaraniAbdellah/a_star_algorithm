077052207a6551120e70aea2e438c769e7a3e7cd
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-13 21:06:39 +0100

    add classe Employee

--- classes.py
+++ classes.py
@@ -1,5 +1,20 @@
 # Start Classes
+'''
+    from Employee import Employee
+        --> create object from another file
+'''
 
+class Employee:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+    
+    
+
+emp = Employee("ahmed", 30, 200000)
+
+print(emp)
 
 
 
