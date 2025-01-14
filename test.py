cd2b97960977b938323e17b1c6fcdd14cbcc4541
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-14 22:20:32 +0100

    start implmente the graph with adjacency list

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-# this for test a peace of code 
+# repeat the graph also with adjacency list
+
+
+
+
+def make_graph(src, dest, list):
+    list[src].append(dest)
+    list[dest].append(src)
+
+
+# here we have just two nodes
+list = [[], []]
+
+
+
+
 
 
