9b1ce6838a3a6a5b46c95eb6f56097557d1d70d3
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-25 20:59:59 +0100

    strt test

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,12 @@
 # Try Implement the A* Algorithm
 
 '''
-    
+    the algorithm use f(n)=g(n)+h(n) to find the shortest path
+    g(n): The cost from the start node to the current node.
+    h(n): The heuristic estimate from the current node to the goal.
+
+    I need in implement to use:
+        Graph, (Min-Heap), Set, Map
 '''
 
 
