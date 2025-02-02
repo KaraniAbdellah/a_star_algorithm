614def550edf6906173a80f84935eb2fcde72f7c
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-02 21:17:12 +0100

    add weighted graph

--- tests/test.py
+++ tests/test.py
@@ -16,21 +16,29 @@
 
 # this is undirected graph
 class Graph:
-    def __init__(self, x):
+    def __init__(self):
         self.graph = {}
 
-    def add_node(u, v, weight):
+    def add_node(self, u, v, weight):
         if u not in self.graph:
             self.graph[u] = []
         if v not in self.graph:
             self.graph[v] = []
 
         # add ele to graph
-        self.graph[u] =  
+        self.graph[u].append((v, weight))
+        self.graph[v].append((u, weight))
+    
+    def display(self):
+        print("hi")
+        for node in self.graph:
+            print(f"{node}: {self.graph[node]}")
+            
         
-
-
-
+myGraph = Graph()
+myGraph.add_node("A", "B", 2)
+myGraph.add_node("B", "C", 2)
+myGraph.display()
 
 
 
