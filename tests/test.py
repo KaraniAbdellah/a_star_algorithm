3fe53398f0d491c4c33dee1918edbb4167d9a602
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-02 20:14:24 +0100

    add wwaeighted graoh

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,11 @@
 # Try Implement the A* Algorithm
 
 '''
+
+    The a* algorithm need the heirstic graph (proposition)
+        --> h(n) = node_level * 2
+        --> node_level(n) is sum of all connect edges to n
+
     the algorithm use f(n)=g(n)+h(n) to find the shortest path
     g(n): The cost from the start node to the current node.
     h(n): The heuristic estimate from the current node to the goal.
