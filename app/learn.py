8fada277d8de0cca0c84339fe0f10ff1fed7a692
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-23 19:51:00 +0000

    add feature for click event to button

--- app/learn.py
+++ app/learn.py
@@ -5,7 +5,6 @@ from tkinter import *
 # Create Object
 root = Tk()
 root.title("learn")
-root.geometry("1000x600")
 
 
 # Widgets
@@ -16,9 +15,9 @@ button1 = Button(root, text="run")
 
 # define grid
 root.rowconfigure(0, minsize=30)  # First row 
-root.rowconfigure(1, weight=1)  # Second row  
+root.rowconfigure(1, weight=1)  # Second row
 root.columnconfigure(0, weight=1)  # First column  
-root.columnconfigure(1, weight=1)  # Second column  
+root.columnconfigure(1, weight=1)  # Second column
 
 
 # Place other widgets in the main grid
@@ -27,19 +26,23 @@ nodes_frame.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
 matrix_frame.grid(row=1, column=1, sticky="nsew", padx=10, pady=10)
 
 
-# Create 20 button for nodes_frame
-for i in range(20):
+# Create 10 row and column for nodes_frame
+for i in range(10):
     nodes_frame.columnconfigure(i, weight=1)
     nodes_frame.rowconfigure(i, weight=1)
 
 
 # Function For Change The Button Content
-def changeContent():
-    button1.config(text="Running")
+def changeContent(btn, row, col):
+    btn.config(bg="#FF7F50", font = ('Sans','10','bold'), fg="white", text=f"{row * 10 + col}")
 
 
-button1.bind("<Button-1>", lambda event: changeContent())
-
+# Update the binding to pass the row and column
+for row in range(10):
+    for col in range(10):
+        button = Button(nodes_frame, width=2, height=2, bg="white")
+        button.grid(row=row, column=col, sticky="nsew")
+        button.bind("<Button-1>", lambda event, btn=button, row=row, col=col: changeContent(btn, row, col))
 
 
 
