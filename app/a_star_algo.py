e9cd9d6b7c5bb21d45a01eaa5bc8f179dd1633ee
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-26 23:41:16 +0100

    add a* algorithm

--- app/a_star_algo.py
+++ app/a_star_algo.py
@@ -1,1 +1,59 @@
-# 
+# oriented graph
+graph = {
+    'S': [('A', 1), ('B', 4)],
+    'A': [('B', 2), ('C', 5), ('G', 12)],
+    'B': [('C', 2)],
+    'C': [('G', 3)]
+}
+
+# heuristic table 
+H_table = {
+    'S': 7, 
+    'A': 6,
+    'B': 4,
+    'C': 2,
+    'G': 0,
+}
+
+# helper function: Path F-cost
+def path_f_cost(path: list[tuple]) -> tuple:
+    g_cost = 0
+    for(node, cost) in path:
+        g_cost += cost
+    last_node = path[-1][0]
+    h_cost = H_table[last_node]
+    f_cost = g_cost + h_cost
+    return (f_cost, last_node)
+
+path = [('S', 0), ('A', 1), ('C', 5)]
+print(path_f_cost(path=path))
+
+
+def a_star_search(graph, start, goal):
+    visited = []
+    queue = [[(start, 0)]]
+    while queue:
+        queue.sort(key=path_f_cost) # sort by f_cost
+        path = queue.pop(0) # get least f_cost
+        node = path[-1][0]
+        if node in visited:
+            continue
+        visited.append(node)
+        if (node == goal):
+            return path
+        else:
+            adjacent_nodes = graph.get(node, [])
+            for (node2, cost) in  adjacent_nodes:
+                new_path = path.copy()
+                new_path.append((node2, cost))
+                queue.append(new_path)
+
+solution = a_star_search(graph=graph, start='S', goal='G')
+print(solution)
+print(f"Cost of Solution is ", path_f_cost(solution)[0])
+
+
+
+
+
+
